fbb60b4918bb932bd4527c63c686b4595a773935
Czenczer <norbertczenczer@gmail.com>
2023-01-31 21:59:26 +0100

    New file aded.

---
+++ booked.js
@@ -0,0 +1,10 @@
+alert('TESZT OLDAL !!!!');
+
+new Booked4us({
+	"CalendarId": "16",
+	"Mode": "button",
+	"ButtonClickMode": "newPage",
+	"ButtonColor": "blue",
+	"ButtonText": "Időpontfoglalás",
+	"SchemaName": "semmelweisegyetem"
+})
